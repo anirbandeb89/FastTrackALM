9d3b81ef290a07924a055c67badb1ae2511a670a
anirbandeb <anirbandeb@192.168.1.6>
2017-07-06 02:23:46 +0530

    Change-Id: I1525203e430ecf89dc575ed2dcf9e34fedb59eb4 Signed-off-by: anirbandeb <anirbandeb@192.168.1.6>

---
+++ FastTrackALM/src/main/java/org/fasttrack/core/ApplicationResource.java
@@ -0,0 +1,14 @@
+package org.fasttrack.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationResource {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApplicationResource.class, args);
+	}
+
+}
